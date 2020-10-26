df58ded677a5ad09020ca52bcd0188d9132b2ea4
lihiitz <lihiitz@gmail.com>
2020-10-26 13:05:35 +0200

    it took me too much time to solve it

--- 101.Symmetric-Tree/symmetricTree.test.js
+++ 101.Symmetric-Tree/symmetricTree.test.js
@@ -1,4 +1,4 @@
-// input: [1,2,2,3,4,4,3] 
+// input1: [1,2,2,3,4,4,3] 
 //     1
 //    / \
 //   2   2
@@ -7,7 +7,7 @@
 
 // output: true
 
-// input: [1,2,2,null,3,null,3] 
+// input2: [1,2,2,null,3,null,3] 
 //     1
 //    / \
 //   2   2
@@ -15,6 +15,18 @@
 //    3    3
 // output: false
 
+//input 3:
+//     1
+//output : true
+
+//input 4:
+//     1
+//    / \
+//   2   2
+//  /     \
+// 3       3
+
+//output: true
 
 
 
@@ -39,9 +51,20 @@ let c = new TreeNode(2, null, a)
 let d = new TreeNode(2, null, b)
 let e = new TreeNode(1, c, d)
 
+let test3One = new TreeNode(1, null, null)
+
+let t4a = new TreeNode(3, null, null)
+let t4b = new TreeNode(3, null, null)
+let t4c = new TreeNode(2, t4a, null)
+let t4d = new TreeNode(2, null, t4b)
+let t4e = new TreeNode(1, t4c, t4d)
+
 const isSymmetric = require('./symmetricTree')
 
 console.log(`test 1: ${isSymmetric(root) === true}`)
 console.log(`test 2: ${isSymmetric(e) === false}`)
+console.log(`test 3: ${isSymmetric(test3One) === true}`)
+console.log(`test 4: ${isSymmetric(t4e) === true}`)
+
 
 
